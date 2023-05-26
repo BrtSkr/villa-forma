fd7a5179a3e36ac2e983e501864d9a8dc44b055d
Bartosz Skrzydelski <brtskr1503@gmail.com>
2023-05-26 07:21:47 +0200

    Finished project
    -Converted images from png to webp
    -Created navbar for both large and small screen sizes
    -Fixed most if not all accessibility issues
    -Added workaround for swiper max-height reach (it didn't allow to scroll to the bottom of the section if section was too big)
    -Added swap for font
    -Small changes here and there

---
+++ readme.md
@@ -0,0 +1,6 @@
+## Setup
+``` npm i ```
+To run project:
+``` npm run dev  ```
+To create a build:
+``` npm run build ```
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,23 @@
 import PhotoSwipeLightbox from 'photoswipe/dist/photoswipe-lightbox.esm.js';
+
 const lightbox = new PhotoSwipeLightbox({
-  gallery: '#my-gallery',
-  children: 'a',
-  pswpModule: () => import('photoswipe/dist/photoswipe.esm.js')
+    gallery: '#my-gallery',
+    children: 'a',
+    pswpModule: () => import('photoswipe/dist/photoswipe.esm.js')
 });
 lightbox.init();
+
+const navHamburger = (action) =>{
+    const openBtn = document.querySelector('.nav_switch');
+    const nav = document.querySelector('.nav_container');
+    if(action === "open"){
+    openBtn.style.visibility = "hidden";
+    nav.style.transform = "translateX(0)";
+    }
+    if(action === "close"){
+        nav.style.transform = "translateX(20rem)";
+        openBtn.style.visibility = "visible";
+    }
+}
+document.querySelector('.nav_switch').addEventListener("click", () => navHamburger("open"));
+document.querySelector('.nav_container__closeBtn').addEventListener('click', () => navHamburger("close"));
