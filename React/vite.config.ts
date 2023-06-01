2255fb49e386d6686d8f316cbb560a0e6c0e0a28
Bartosz Skrzydelski <brtskr1503@gmail.com>
2023-06-01 07:03:46 +0200

    Added PWA support in React

--- React/vite.config.ts
+++ React/vite.config.ts
@@ -1,7 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import {VitePWA} from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    VitePWA({
+      strategies: 'generateSW',
+      registerType: 'autoUpdate',
+      includeAssets: ['./src/assets/font/ReadexPro-Regular.ttf', './src/assets/img/logo.png'],
+      manifest: {
+        name: 'Villa Forma',
+        short_name: 'Vforma',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#000000',
+        background_color: '#000000',
+        icons: [{
+          src: './pwa/apple-icon-180.png',
+          sizes: '180x180'
+        },
+        {
+          src: './pwa/manifest-icon-192.maskable.png',
+          sizes: '192x192',
+        },
+        {
+          src: './pwa/manifest-icon-512.maskable.png',
+          sizes: '512x512'
+        },
+        {
+          src: './pwa/manifest-icon-192.maskable.png',
+          sizes: '192x192'
+        },
+        {
+          src: './pwa/manifest-icon-512.maskable.png',
+          sizes: '512x512'
+        },
+        ]
+        
+      }
+      }
+    )
+  ],
 })
--- readme.md
+++ readme.md
@@ -13,5 +13,6 @@ or
 ### To create a build:
 * ``` npm run build ```
 ## Details
+* PWA support is only added to React build, remember with each build copy pwa folder from assets to dist
 * Bundler that was used: in Vanilla - Parcel, React - Vite
 * Used libraries: Swiper.js (Only in vanilla), photoswipe (Both)
